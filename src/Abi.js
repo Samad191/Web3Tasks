ea538b3d530695c1c276ece37737a49cef33f96c
AbdulSamad <samad13354@gmail.com>
2022-06-15 18:43:07 +0500

    feat: tx speed, tx from hash and event listener added

---
+++ src/Abi.js
@@ -0,0 +1,187 @@
+export const simpleStorageAbi = [
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_initialMessage",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "int256",
+          "name": "a",
+          "type": "int256"
+        },
+        {
+          "internalType": "int256",
+          "name": "b",
+          "type": "int256"
+        }
+      ],
+      "name": "doMath",
+      "outputs": [],
+      "stateMutability": "pure",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "initialMessage",
+          "type": "string"
+        }
+      ],
+      "name": "inbox",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "message",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "newMessage",
+          "type": "string"
+        }
+      ],
+      "name": "setMessage",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
+
+export const lotteryContractAbi = [
+  
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [],
+      "name": "enter",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getContractBalance",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getPlayers",
+      "outputs": [
+        {
+          "internalType": "address[]",
+          "name": "",
+          "type": "address[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "manager",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "pickWinner",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    }
+  
+  ];
+
+
+
+  export  const eventListenerContractAbi =  [
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "sender",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "receiver",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "transferEvent",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address payable",
+          "name": "receiver",
+          "type": "address"
+        }
+      ],
+      "name": "transfer",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "payable",
+      "type": "function"
+    }
+  ]
--- src/App.js
+++ src/App.js
@@ -1,24 +1,125 @@
 import logo from './logo.svg';
 import './App.css';
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+import { useWeb3React } from '@web3-react/core'
+import { InjectedConnector } from '@web3-react/injected-connector'
+import { simpleStorageAbi, lotteryContractAbi, eventListenerContractAbi } from './Abi'
+import Web3 from 'web3';
+import { useEffect } from 'react';
+
+export const injectedConnector = new InjectedConnector({
+  supportedChainIds: [1,3,4,5,42]
+})
+
+const web3 = new Web3(Web3.givenProvider)
+
+function getLibrary(provider){
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 12000;
+  return library
+}
+
+
+const Wallet = () => {
+
+const eventListener = () => {
+  const contractAddr = '0xBCB54052E11Ab15fd426842CfD42020c1af2ee4e';
+  const myContract = new web3.eth.Contract(eventListenerContractAbi, contractAddr);
+  console.log('My contract', myContract);
+  myContract.getPastEvents('transferEvent')
+    .then(res => console.log('a',res))
+    .catch(err => console.log(err));
+
+  myContract.events.allEvents(res => console.log('b',res));
+
+  myContract.events.transferEvent({
+    // filter: {myIndexedParam: [20,23], myOtherIndexedParam: '0x123456789...'}, // Using an array means OR: e.g. 20 or 23
+    fromBlock: 0
+}, function(error, event){ console.log(event); })
+.on('data', function(event){
+    console.log('c',event); // same results as the optional callback above
+})
+.on('changed', function(event){
+  console.log('abc')
+    // remove event from local database
+})
+.on('error', () => console.log('error'));
+
+}
+
+  const triggerEvent = async () => {
+    const gas = '91212'
+    const contractAddr = '0xBCB54052E11Ab15fd426842CfD42020c1af2ee4e';
+    const myContract = new web3.eth.Contract(eventListenerContractAbi, contractAddr);
+    
+    const result = await myContract.methods.transfer('0xd82b09990f96EDBd6e7731C67e7ea4c9b01AF150').send({ from: account, gas, value: 2})
+ 
+  }
+
+  const readTxFromHash = async () => {
+    const hash = "0x0ec03078eafc5e9388445eac8130aa5f4182bcaaf7a7274207dfc505ff393bae"
+    const res = await web3.eth.getTransaction(hash);
+    console.log('res ---->>>>', res)
+    console.log('This the amount transfered', res.value)
+  }
+
+  const speedUpTx = async () => {
+    const addressTo = '0xd82b09990f96EDBd6e7731C67e7ea4c9b01AF150';
+    console.log('account', account)
+    const nonce = await web3.eth.getTransactionCount(account, 'latest');
+
+    console.log('Nonce', nonce);
+    let rawTx = {
+      from: account,
+      to: addressTo,
+      value: web3.utils.toWei('0.1', 'ether'),
+      gas: '900000000',
+      chainId: '4',
+      nonce: nonce.toString()
+    }
+
+    web3.eth.sendTransaction(rawTx).on('transactionHash', (txHash) => {
+      console.log('hash of new tx', txHash)
+    })
+
+  }
+  
+  const { chainId, account, activate } = useWeb3React()      
+
+  const onClick = () => {
+    activate(injectedConnector)
+  }
+
+
+  return (
+    <div>
+      {chainId}
+      <br />
+      {account}
+      <br />
+
+      <h2> EVENT LISTENER </h2>
+      <button  onClick={onClick} > Activate MM </button>
+      <br /> <br />
+ 
+
+      <button onClick={eventListener} >Event Listener</button>
+      <button onClick={triggerEvent} >Trigger event</button>
+      <br />
+      <br />
+      <button onClick={readTxFromHash} >Tx Hash</button>
+      <br /> <br />
+      <button onClick={speedUpTx} >Speed up Tx</button>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Web3ReactProvider getLibrary={getLibrary} >
+         <Wallet />
+      </Web3ReactProvider>
   );
 }
 
